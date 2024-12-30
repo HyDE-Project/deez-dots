9fb43c366c1f1c640a0855a027e910310d139eec
kRHYME7 <alampayk@gmail.com>
2024-12-30 20:33:57 +0800

    feat: git handler

--- deez.py
+++ deez.py
@@ -10,14 +10,7 @@ import shutil
 from typing import List, Optional, Dict, Any
 import logging
 import time
-
-CFG_BACKUP_DIR = os.path.join(
-    os.getenv("XDG_CACHE_HOME", os.path.expanduser("~/.cache")),
-    "deez-dots",
-    "backup",
-    time.strftime("%Y%m%d%H%M%S"),
-)
-logging.basicConfig(level=logging.INFO)
+import argparse
 
 
 def pacman_query(package_managers: List[str], package: str) -> bool:
@@ -90,7 +83,7 @@ def write_file(act: str, src: str, tgt: str, paths: List[str]) -> None:
     """Write files based on the specified action."""
 
     def backup_target(src: str, tgt: str, paths: List[str]) -> None:
-        logging.getLogger().setLevel(logging.CRITICAL)
+        # logging.getLogger().setLevel(logging.CRITICAL)
         """Execute backup of target paths."""
         for pth in paths:
             target_path = os.path.join(tgt, pth)
@@ -119,7 +112,6 @@ def write_file(act: str, src: str, tgt: str, paths: List[str]) -> None:
                     shutil.copy2(target_path, backup_path)
             else:
                 logging.warning("Target path does not exist: %s", target_path)
-        logging.getLogger().setLevel(logging.INFO)
 
     def preserve_target(src: str, tgt: str, paths: List[str]) -> None:
         """Preserve existing target paths."""
@@ -202,13 +194,17 @@ def read_toml(file_path: str) -> Dict[str, Any]:
 
 
 def filter_deps(
-    pac_man: List[str], dependency: Dict[str, List[str]]
+    package_manager: List[str],
+    dependency: Dict[str, List[str]],
+    filtered_deps: Dict[str, List[str]] = None,
 ) -> Dict[str, List[str]]:
     """Filter dependencies based on available package managers."""
-    filtered_deps = {}
+    if filtered_deps is None:
+        filtered_deps = {}
+
     seen_packages = set()
-    for manager in pac_man:
-        for dep_manager, dep_list in dependency.items():
+    for dep_manager, dep_list in dependency.items():
+        for manager in package_manager:
             dep_managers = dep_manager.split(",")
             if manager in dep_managers:
                 if manager not in filtered_deps:
@@ -236,19 +232,25 @@ def fetch_all_deps(data: Dict[str, Any]) -> Dict[str, List[str]]:
     return all_deps
 
 
-def resolve_package_managers():
-    global package_manager
-    if not package_manager or package_manager in ["", "auto", None, [""]]:
-        package_manager = available_package_managers
-    Deps = filter_deps(package_manager, fetch_all_deps(main_config))
-    logging.info("Expected package manager: %s", package_manager)
-    if not check_dependencies(Deps):
-        logging.error("Missing dependencies: %s", Deps)
+def resolve_package_managers(pac_man):
+    if not pac_man or pac_man in ["", "auto", None, [""], ["auto"]]:
+        pac_man = available_package_managers
+
+    if not any(manager in available_package_managers for manager in pac_man):
+        logging.error("Specified package manager(s) not available: %s", pac_man)
         sys.exit(1)
+
+    if not pac_man:
+        logging.error("No package manager available.")
+        sys.exit(1)
+    return pac_man
+
+    logging.info("Expected package manager: %s", pac_man)
 
 
 def deploy_files(files: List[Dict[str, Any]]):
     for file_action in files:
+        global action
         action = file_action.get("action", default_action)
         source_root = file_action.get("source_root")
         if source_root and "$" in source_root:
@@ -273,16 +275,95 @@ def deploy_files(files: List[Dict[str, Any]]):
         write_file(action, source_root, target_root, paths)
 
 
-if __name__ == "__main__":
-    # if len(sys.argv) < 2:
-    # logging.error("Usage: python3 deez-dots.py <path_to_toml_file>")
-    # sys.exit(1)
+def handle_git(url: str):
+    """Handle Git repository information."""
 
-    if len(sys.argv) < 2:
-        config_file_path = os.path.expanduser("~/The HyDE Project/HyDE/hyde.toml")
+    def git_clone(url: str, target_dir: str):
+        subprocess.run(["git", "clone", "--depth", "1", url, target_dir], check=True)
+
+    def git_fetch(repo_path: str):
+        subprocess.run(["git", "-C", repo_path, "fetch", "--all"], check=True)
+
+    def git_checkout(repo_path: str, branch: str):
+        subprocess.run(["git", "-C", repo_path, "checkout", branch], check=True)
+
+    target_branch = main_config.get("git_branch", "main")
+    version = main_config.get("version", None)
+    clone_dir = os.path.join(
+        os.getenv("XDG_CACHE_HOME", os.path.expanduser("~/.cache")),
+        "deez-dots",
+        "clones",
+    )
+
+    # Extract repository owner and name from URL
+    repo_owner = url.split("/")[-2]
+    repo_name = url.split("/")[-1].replace(".git", "")
+    global source_root_path
+    if version:
+        source_root_path = os.path.join(
+            clone_dir, f"{repo_owner.lower()}.{repo_name.lower()}.{version}"
+        )
     else:
-        arg_1 = sys.argv[1]
-        config_file_path = os.path.realpath(arg_1)
+        source_root_path = os.path.join(
+            clone_dir, f"{repo_owner.lower()}.{repo_name.lower()}"
+        )
+
+    # Clone the repository if it doesn't exist
+    if not os.path.exists(source_root_path):
+        git_clone(url, source_root_path)
+    # Fetch the latest changes
+    git_fetch(source_root_path)
+    # Checkout the specified branch
+    git_checkout(source_root_path, target_branch)
+
+    branches = subprocess.run(
+        ["git", "-C", source_root_path, "branch", "-r"],
+        check=True,
+        text=True,
+        capture_output=True,
+    ).stdout.splitlines()
+    # Get remote branches
+    remote_branches = subprocess.run(
+        ["git", "-C", source_root_path, "branch", "-r"],
+        check=True,
+        text=True,
+        capture_output=True,
+    ).stdout.splitlines()
+    print(f"Remote Branches: {remote_branches}")
+
+    # Get tags
+    tags = subprocess.run(
+        ["git", "-C", source_root_path, "tag"],
+        check=True,
+        text=True,
+        capture_output=True,
+    ).stdout.splitlines()
+
+    print(f"Repository Owner: {repo_owner}")
+    print(f"Repository Name: {repo_name}")
+    print(f"Branches: {branches}")
+    print(source_root_path)
+    print(f"Tags: {tags}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Deez Dots Deployment Script")
+    parser.add_argument(
+        "-c", "--config", type=str, help="Path to the dots TOML configuration file"
+    )
+    parser.add_argument(
+        "-s", "--source", type=str, help="Path to the source root directory"
+    )
+    args = parser.parse_args()
+
+    if args.config:
+        config_file_path = os.path.realpath(os.path.expanduser(args.config))
+    else:
+        config_file_path = os.path.expanduser("~/.config/hyde/dots.toml")
+
+    global available_package_managers
+    global main_config
+    global source_root_path
 
     source_root_path = os.path.dirname(config_file_path)
     if not os.path.isfile(config_file_path):
@@ -292,14 +373,27 @@ if __name__ == "__main__":
     logging.info("Reading file: %s", config_file_path)
     available_package_managers = available_managers()
     main_config = read_toml(config_file_path)
+    git_url = main_config.get("git")
+    if git_url:
+        handle_git(git_url)
+    print(source_root_path)
+
     mainAction = main_config.get("default_action")
     start_cmd = main_config.get("start_command")
-    package_manager = main_config.get("package_manager", available_package_managers)
     end_cmd = main_config.get("end_command")
+    package_manager = main_config.get("package_manager", available_package_managers)
     declared_dots = main_config.get("dots", [])
 
     # Dependency resolution
-    resolve_package_managers()
+    package_manager = resolve_package_managers(package_manager)
+
+    # Handle all dependencies
+    # TODO make func to install all dependenciess
+    all_dependencies = filter_deps(package_manager, fetch_all_deps(main_config))
+    if not check_dependencies(all_dependencies):
+        logging.error("Missing dependencies: %s", all_dependencies)
+        sys.exit(1)
+
     execute_commands(start_cmd)
     logging.info("____________________________")
 
@@ -308,26 +402,36 @@ if __name__ == "__main__":
         logging.error("No dots declared in the file.")
         sys.exit(1)
 
+    global CFG_BACKUP_DIR
+    CFG_BACKUP_DIR = os.path.join(
+        os.getenv("XDG_CACHE_HOME", os.path.expanduser("~/.cache")),
+        "deez-dots",
+        "backup",
+        time.strftime("%Y%m%d%H%M%S"),
+    )
+    logging.basicConfig(level=logging.INFO)
     if not os.path.exists(CFG_BACKUP_DIR):
         os.makedirs(CFG_BACKUP_DIR, exist_ok=True)
 
     # evaluate dotfiles
+    global dot_name
     for dot_name in declared_dots:
         logging.info("Deploying %s", dot_name)
-        dotData = main_config.get(dot_name)
-        default_pre_cmd = dotData.get("pre_command")
+        dot_data = main_config.get(dot_name)
+        default_pre_cmd = dot_data.get("pre_command")
         if isinstance(default_pre_cmd, str):
             default_pre_cmd = [default_pre_cmd]
 
-        default_post_cmd = dotData.get("post_command")
+        default_post_cmd = dot_data.get("post_command")
         if isinstance(default_post_cmd, str):
             default_post_cmd = [default_post_cmd]
 
-        default_action = dotData.get("action", mainAction)
-        files = dotData.get("files")
+        global default_action
+        default_action = dot_data.get("action", mainAction)
+        files = dot_data.get("files")
 
-        deps = dotData.get("dependency")
-        deps = filter_deps(package_manager, deps)
+        get_deps = dot_data.get("dependency")
+        deps = filter_deps(package_manager, get_deps)
         if not check_dependencies(deps):
             logging.warning("Skipping due to missing dependencies: %s", deps)
             continue
@@ -337,3 +441,9 @@ if __name__ == "__main__":
 
         execute_commands(default_post_cmd)
         logging.info("____________________________")
+    execute_commands(end_cmd)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
